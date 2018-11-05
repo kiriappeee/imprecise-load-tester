57d643106128953568dc526d147982d764d1c617
Adnan Issadeen <eddyrox1@hotmail.com>
2018-11-05 18:35:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Basic and imprecise load testing
+
+## Usage instructions
+
+* Make sure you've installed `python3` and `pipenv`
+* Clone the repo
+* Run `pipenv install --three`
+* Copy `tests.example.yaml` to `tests.yaml`
+* Edit `tests.yaml` as needed (explaining of values given below)
+* Run `pipenv run python load.py`
+* Check the generated `-results.txt` files for your results
+
+## `tests.yaml` options
+
+`tests` is the top level array that must be present in the file. Each item has the following
+elements:
+
+* `name`: Used to generate the results files (`name-results.txt`). Program does not check for uniqueness.
+* `url`: Url to load test
+* `iterations`: Number of times to load the url
+* `enabled` (OPTIONAL): Defaults to `true`. Use `enabled: false` if you want to disable a test.
---
+++ load.py
@@ -0,0 +1,89 @@
+import multiprocessing
+from time import sleep
+import random
+import requests
+from datetime import datetime
+import yaml
+import math
+
+def make_request(request_url):
+    """Make a request."""
+    return requests.get(request_url)
+
+
+def run_test(test_url, results_arr, time_taken_arr, index):
+    t1 = datetime.now()
+    res = make_request(test_url)
+    t2 = datetime.now()
+    total_time = t2 - t1
+    results_arr[index] = res.status_code
+    time_taken_arr[index] = float("{}.{}".format(total_time.seconds, total_time.microseconds))
+
+
+
+def percentile(N, percent, key=lambda x:x):
+    """
+    Find the percentile of a list of values.
+
+    @parameter N - is a list of values. Note N MUST BE already sorted.
+    @parameter percent - a float value from 0.0 to 1.0.
+    @parameter key - optional key function to compute value from each element of N.
+
+    @return - the percentile of the value
+    Credit due to http://code.activestate.com/recipes/511478-finding-the-percentile-of-the-values/
+    """
+    if not N:
+        return None
+    k = (len(N)-1) * percent
+    f = math.floor(k)
+    c = math.ceil(k)
+    if f == c:
+        return key(N[int(k)])
+    d0 = key(N[int(f)]) * (c-k)
+    d1 = key(N[int(c)]) * (k-f)
+    return d0+d1
+
+if __name__ == "__main__":
+    f = open('tests.yaml', 'r')
+    test_details = yaml.load(f.read())
+    for test in test_details['tests']:
+        test_name = test['name']
+        if 'enabled' in test and not test['enabled']:
+            print("skipping {}".format(test_name))
+            continue
+        w = open("{}-results.txt".format(test_name), 'w')
+        test_url = test['url']
+        iterations = test['iterations']
+        print("Running {} for {}".format(test_name, test_url))
+        results = multiprocessing.Array('i', iterations)
+        time_taken = multiprocessing.Array('f', iterations)
+        for i in range(0, iterations):
+            multiprocessing.Process(target=run_test, args=(test_url, results, time_taken, i)).start()
+            print(i)
+            sleep(random.randrange(1, 11, 1) / 10)
+        while(multiprocessing.active_children()):
+            print("waiting for children to finish")
+            print(multiprocessing.active_children())
+            sleep(1)
+        print("done")
+        w.write("""URL - {}
+
+Average Time - {}
+50th Percentile - {}
+99th Percentile - {}
+Max time - {}
+Min time - {}
+
+Times -
+{}
+
+Results -
+{}""".format(test_url, 
+             str(sum(time_taken) / iterations),
+             percentile(sorted(time_taken[:]), 0.5),
+             percentile(sorted(time_taken[:]), 0.9),
+             max(time_taken),
+             min(time_taken),
+             "\n".join([str(t) for t in time_taken[:]]),
+             "\n".join([str(r) for r in results[:]])))
+        w.close()
